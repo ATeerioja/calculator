4541b81fb2bf687eaa750c6018b581500fbef63a
ATeerioja <teerioja.opiskelu@gmail.com>
2024-07-23 13:55:10 +0300

    Operate function

--- calculator.js
+++ calculator.js
@@ -1,6 +1,23 @@
 //Simple math operators
+const num1 = "";
+const num2 = "";
+const operator = "";
+
 
 let add = (num1, num2) => num1 + num2;
 let subtract = (num1, num2) => num1 - num2;
 let multiply = (num1, num2) => num1 * num2;
 let divide = (num1, num2) => num1 / num2;
+
+function operate(operator, num1, num2) {
+  //Check which operator to use
+  if(operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
